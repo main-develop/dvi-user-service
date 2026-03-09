3d722e0c9f25ebfadb2e92213b1247bc97021e20
main-develop <main.develop@protonmail.com>
2026-03-09 18:59:35 +0100

    feat: Change default length of the User model fields

--- users/models.py
+++ users/models.py
@@ -1,15 +1,17 @@
 import uuid
 
 from django.contrib.auth.models import AbstractUser
-from django.db.models import DateTimeField, EmailField, UUIDField
+from django.db.models import DateTimeField, EmailField, UUIDField, CharField
+from django.core.validators import MinLengthValidator
 
 
 class User(AbstractUser):
     id = UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     first_name = None
     last_name = None
-    email = EmailField(unique=True, blank=False, null=False)
-    pending_email = EmailField(blank=True, null=True)
+    username = CharField(max_length=25, validators=[MinLengthValidator(3)], unique=True, blank=False, null=False)
+    email = EmailField(max_length=254, unique=True, blank=False, null=False)
+    pending_email = EmailField(max_length=254, blank=True, null=True)
     deletion_scheduled_at = DateTimeField(
         blank=True,
         null=True,
