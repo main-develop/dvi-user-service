ecdcd838dcd7c16104cb0cbf25fde8b4de993274
main-develop <main.develop@protonmail.com>
2026-04-16 23:30:25 +0200

    fix: Add default env variables for the email settings

--- dvi_user_service/settings/email.py
+++ dvi_user_service/settings/email.py
@@ -25,8 +25,8 @@ DJOSER = {
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_USE_TLS = True
-EMAIL_HOST = env("EMAIL_HOST")
-EMAIL_PORT = env("EMAIL_PORT")
-EMAIL_HOST_USER = env("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
-DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL")
+EMAIL_HOST = env("EMAIL_HOST", default="smtp.example.com")
+EMAIL_PORT = env("EMAIL_PORT", default="587")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER", default="example@domain.com")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD", default="")
+DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL", default="")
