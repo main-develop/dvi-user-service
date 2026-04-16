30097992c4d4d215324a1861993bc1cbef4b4866
main-develop <main.develop@protonmail.com>
2026-04-16 16:14:48 +0200

    test: Increase tests coverage for the login logic

--- users/tests/test_views/test_auth_views.py
+++ users/tests/test_views/test_auth_views.py
@@ -22,6 +22,33 @@ def test_login_success(api_client, user):
 
 
 @pytest.mark.django_db
+def test_login_user_does_not_exist(api_client, user):
+    data = {
+        "username_or_email": "non-existent@example.com",
+        "password": "testpassword123",
+        "remember_me": False,
+    }
+    response = api_client.post(path=reverse("auth_login"), data=data, format="json")
+
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+
+@pytest.mark.django_db
+def test_login_user_has_unusable_password(api_client, user):
+    user.set_unusable_password()
+    user.save()
+
+    data = {
+        "username_or_email": user.email,
+        "password": "testpassword123",
+        "remember_me": False,
+    }
+    response = api_client.post(path=reverse("auth_login"), data=data, format="json")
+
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+
+@pytest.mark.django_db
 def test_login_invalid_credentials(api_client):
     data = {"username_or_email": "wrong", "password": "wrong"}
     response = api_client.post(path=reverse("auth_login"), data=data, format="json")
