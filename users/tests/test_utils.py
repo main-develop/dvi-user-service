a41b3fb4a32e0c243706b7244afe5fda182dd8f0
main-develop <main.develop@protonmail.com>
2026-04-17 02:56:10 +0200

    test: Cover other user's sessions

--- users/tests/test_utils.py
+++ users/tests/test_utils.py
@@ -6,18 +6,32 @@ from django.contrib.sessions.backends.db import SessionStore
 from django.contrib.sessions.models import Session
 
 from users.utils import revoke_all_user_sessions
+from factories import UserFactory
 
 
 @pytest.mark.django_db
 def test_revoke_all_user_sessions(user):
-    session_key = "testkey"
-    session_store = SessionStore(session_key)
+    session_store = SessionStore()
     session_store["_auth_user_id"] = str(user.pk)
     session_store.save()
+    session_key = session_store.session_key
 
     revoke_all_user_sessions(user)
 
     assert not Session.objects.filter(session_key=session_key).exists()
+
+
+@pytest.mark.django_db
+def test_revoke_all_user_sessions_does_not_affect_other_users(user):
+    other_user = UserFactory()
+    session_store = SessionStore()
+    session_store["_auth_user_id"] = str(other_user.pk)
+    session_store.save()
+    session_key = session_store.session_key
+
+    revoke_all_user_sessions(user)
+
+    assert Session.objects.filter(session_key=session_key).exists()
 
 
 @pytest.mark.django_db
@@ -32,5 +46,5 @@ def test_revoke_all_user_sessions_force_delete_on_error(user, caplog):
             revoke_all_user_sessions(user)
 
     assert len(caplog.records) == 1
-    assert "Failed to decode/delete session" in caplog.records[0].message
-    bad_session.delete.assert_called_once()
+    assert "Failed to decode session" in caplog.records[0].message
+    bad_session.delete.assert_not_called()
