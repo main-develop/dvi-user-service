ccc791d6c2fbdbec5c29b76151702cc3a8f5f326
main-develop <main.develop@protonmail.com>
2026-04-19 02:07:13 +0200

    chore: Run test tasks locally

--- dvi_user_service/settings/test.py
+++ dvi_user_service/settings/test.py
@@ -21,3 +21,6 @@ CACHES = {
 EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
 
 PASSWORD_HASHERS = ["django.contrib.auth.hashers.MD5PasswordHasher"]
+
+CELERY_TASK_ALWAYS_EAGER = True
+CELERY_TASK_EAGER_PROPAGATES = True
